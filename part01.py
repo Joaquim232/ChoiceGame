dd833e1eae207dda7f9329cc37c660ac477a76b4
Joaquim Miguel <74877588+Joaquim232@users.noreply.github.com>
2020-11-26 14:17:45 -0300

    Add files via upload

---
+++ part01.py
@@ -0,0 +1,21 @@
+print('Olá! Bem vindo ao jogo de perguntas! Aperte enter para começar!')
+input()
+print('Dica: Sempre aperte enter quando preciso!')   
+nome = input('Ok! Estamos no jogo! Primeiramente, qual o seu nome?: ')
+print('Seja bem vindo, {}!'.format(nome))
+p1 = input('Primeira pergunta: Se estivesse numa sala preso com um computador antigo, o que faria? ')
+if p1 == 'Mexer no computador' 'Procurar informações':
+    print ('Ok, próxima pergunta...')
+else:
+    print ('Ok, próxima pergunta...')
+p2 = input('Vocẽ viu alguém vindo aonde você está, o que vocẽ faz? ')
+if p2 == 'Me esconderia':
+    print ('O óbvio né rs')
+else:
+    print('Sério?')
+p3 = input('Esta pessoa é um homem alto com uma seringa na mão! E agora?')
+if p3 == 'Eu correria':
+    print('Melhor correr mesmo!')
+else:
+    print('Parece que ele te atingiu!')
+print('Bem, esse é um pouco do que estou criando, Depois continuarei!')
